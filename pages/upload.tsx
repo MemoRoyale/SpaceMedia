ab3b21a4d2e940d3c81c149aae9dd2d2bdef476e
Osama <memo1228765@gmail.com>
2022-09-29 16:44:16 +0800

    d

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -93,7 +93,6 @@ const Upload  = () => {
         <div>
           <div>
             <p className='text-2xl font-bold'>Upload Video</p>
-            <p className='text-md text-gray-400 mt-1'>Post a video to your account</p>
           </div>
           <div className='border-dashed rounded-xl border-4 border-gray-200 flex flex-col
           justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-purple-300 hover:bg-gray-100'>
