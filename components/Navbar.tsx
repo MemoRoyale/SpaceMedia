1556474142cc39b3db98aab0533e13b6776976f2
Osama <memo1228765@gmail.com>
2022-10-05 02:33:07 +0800

    test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = () => {
 
   return (
     <div className='w-full flex justify-between item-center border-b-2 border-gray-200 py-2 px-4'>
-         <Link href={`/profile/${userProfile?.id}`}>
+         <Link href={`/profile/${userProfile?._id}`}>
         <div className='w-[180px] md:w-[160px] md:h-[30px] h-[40px]'>
           <Image
             className='cursor-pointer'
