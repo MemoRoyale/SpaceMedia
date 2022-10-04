b9430d3c3c9ab3e160cd0a561fa6e3812bc58078
Osama <memo1228765@gmail.com>
2022-10-05 01:55:00 +0800

    test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -35,7 +35,7 @@ const Navbar = () => {
 
   return (
     <div className='w-full flex justify-between item-center border-b-2 border-gray-200 py-2 px-4'>
-         <Link href='/'>
+         <Link href={`/profile/${userProfile?.id}`}>
         <div className='w-[180px] md:w-[160px] md:h-[30px] h-[40px]'>
           <Image
             className='cursor-pointer'
