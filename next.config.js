0f54fc5484db564c9c0587f4bf638ca79e91f946
Osama <memo1228765@gmail.com>
2022-11-15 05:57:29 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');  
-const nextConfig = withPWA({
-  
+
+const nextConfig = {
   typescript:{
     ignoreBuildErrors: true,
 
   },
+
+  
+   
   
   reactStrictMode: true,
   swcMinify: true,
@@ -15,6 +17,6 @@ const nextConfig = withPWA({
       'lh3.googleusercontent.com'
     ],
   }
-});
+}
 
 module.exports = nextConfig
