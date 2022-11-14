f5a3f71e6b5698aca83b21cf2c244ae5f9e20c35
Osama <memo1228765@gmail.com>
2022-11-15 05:42:08 +0800

    /

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,7 @@ interface IProps {
 
 const Home = ({videos}:IProps) => {
   console.log(videos);
-  
   return (
-    
     
     <div className='flex flex-col gap-10 videos h-full'>
       {videos.length ?(
@@ -27,15 +25,7 @@ const Home = ({videos}:IProps) => {
     </div>
   )
 }
-function registerServiceWorker(): void {
-  if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('service-worker.jsx')
-          .then((registration) =>
-              console.log(`Service Worker registration complete, scope: '${registration.scope}'`))
-          .catch((error) =>
-              console.log(`Service Worker registration failed with error: '${error}'`));
-  }
-}
+
 
 export const getServerSideProps = async ({
   query: { topic },
@@ -52,6 +42,6 @@ export const getServerSideProps = async ({
     props: { videos: response.data },
   };
 };
-registerServiceWorker();
+
 export default Home
 
